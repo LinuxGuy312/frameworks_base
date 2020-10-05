983664d088a6af2b2d066cf38cd282824434f7eb
DennySPb <dennyspb@gmail.com>
2020-10-05 12:52:54 +0300

    SystemUI: Optional haptic feedback on back gesture [1/2]
    Squashed with:
    
        From: DennySPb <dennyspb@gmail.com>
        Date: Wed, 28 Jun 2023 09:42:28 +0300
        Subject: Disable haptic in new back gesture affordance
    
        it's already handled by our custom impl
    
        Change-Id: Ie78b127d0369f6111bcd30e9879108f2c27378ba
        Signed-off-by: DennySPb <dennyspb@gmail.com>
    
    Change-Id: I3bd949cde08ac9a0513e05f2356e84fde07ed767
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    [nurkeinneid - Update to Android 14 QPR1]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6225,6 +6225,11 @@ public final class Settings {
          */
 
         /**
+         * @hide
+         */
+        public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6363,6 +6368,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION);
             PRIVATE_SETTINGS.add(SCREEN_FLASH_NOTIFICATION_COLOR);
             PRIVATE_SETTINGS.add(DEFAULT_DEVICE_FONT_SCALE);
+            PRIVATE_SETTINGS.add(BACK_GESTURE_HAPTIC);
         }
 
         /**
